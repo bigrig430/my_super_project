346a7d9224c346fd91dbbca19507560306a78c60
Your Name <you@example.com>
2026-08-10 20:40:30 +1000

    sync

--- src/main.cpp
+++ src/main.cpp
@@ -163,7 +163,7 @@ int main() {
     uv_tcp_init(loop, &server);
 
     struct sockaddr_in addr;
-    int port = 9002; // change here if needed
+    int port = 49152; // change here if needed
     uv_ip4_addr("127.0.0.1", port, &addr);
 
     int r = uv_tcp_bind(&server, (const struct sockaddr*)&addr, 0);
